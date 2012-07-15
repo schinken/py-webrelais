66673a339bdb9896b6e4f2b53c9db6352a19d37d
schinken <schinken@hackerspace-bamberg.de>
2012-07-15 18:58:12 +0200

    Using new permission check and setMask method

--- py-webrelais.py
+++ py-webrelais.py
@@ -13,28 +13,35 @@ def page_main():
 
 
 @app.route("/relais", methods=["GET"])
-@app.route("/relais/<int:port>", methods=["GET"])
-@check_permissions
+@app.route("/relais/<int:relais>", methods=["GET"])
 @output_handler
-def get_relais( port=None ):
-    return pp.getPin( port )
+def get_relais( relais=None ):
+    return pp.getPin( relais )
 
 
 @app.route("/relais", methods=["POST"])
-@app.route("/relais/<int:port>", methods=["POST"])
-@check_permissions
+@app.route("/relais/<int:relais>", methods=["POST"])
 @output_handler
-def set_relais( port=None ):
-    pp.setPin( port )
+def set_relais( relais=None ):
+
+    mask = get_relais_mask(relais, True)
+    if all(x is None for x in mask):
+        return auth_required()
+
+    pp.setMask( mask )
     return True
 
 
 @app.route("/relais", methods=["DELETE"])
-@app.route("/relais/<int:port>", methods=["DELETE"])
-@check_permissions
+@app.route("/relais/<int:relais>", methods=["DELETE"])
 @output_handler
-def reset_relais( port=None ):
-    pp.resetPin( port )
+def reset_relais( relais=None ):
+
+    mask = get_relais_mask(relais, False)
+    if all(x is None for x in mask):
+        return auth_required()
+
+    pp.setMask( mask )
     return True
 
 
