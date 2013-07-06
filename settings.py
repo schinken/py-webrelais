90476e4b8c2ed79b226e4352887750e6803adddc
schinken <schinken@hackerspace-bamberg.de>
2013-07-06 19:38:04 +0200

    added usb relais configuration

--- settings.py
+++ settings.py
@@ -1,5 +1,3 @@
-
-# host is optional
 door_credentials = {'user':'username', 'pass':'password'}
 user_credentials = {'user':'john', 'pass': 'doe'}
 
@@ -10,3 +8,9 @@ port_permissions[0] = [door_credentials]
 port_permissions[1] = [door_credentials]
 port_permissions[2] = [door_credentials, user_credentials]
 port_permissions[3] = [user_credentials]
+
+
+relais = [
+    {'serial': 'A702FJ36', 'start': 0, 'end': 7,  'relais': 8},
+    {'serial': 'A702FJ36', 'start': 8, 'end': 15, 'relais': 8},
+]
